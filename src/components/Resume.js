1e50bc7ae39d1a6ff589c8c8485d63b77669e609
Mcas4150 <mikecassidy0000@gmail.com>
2019-08-23 13:29:04 -0400

    cleaning

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -13,18 +13,7 @@ export default class Resume extends Component {
           <Header>
             <H1>
               <ScrollText title="Education" />
-              {/* <Word>
-                <Letter>E</Letter>
-                <Letter>d</Letter>
-                <Letter>u</Letter>
-                <Letter>c</Letter>
-                <Letter>a</Letter>
-                <Letter>t</Letter>
-                <Letter>i</Letter>
-                <Letter>o</Letter>
-                <Letter>n</Letter>
-              </Word> */}
-            </H1>
+0--            </H1>
           </Header>
           <Content>
             <P>
@@ -38,18 +27,6 @@ export default class Resume extends Component {
           <Header>
             <H1>
               <ScrollText title="Experience" />
-              {/* <NewWord>
-                <Letter>E</Letter>
-                <Letter>x</Letter>
-                <Letter>p</Letter>
-                <Letter>e</Letter>
-                <Letter>r</Letter>
-                <Letter>i</Letter>
-                <Letter>e</Letter>
-                <Letter>n</Letter>
-                <Letter>c</Letter>
-                <Letter>e</Letter>
-              </NewWord> */}
             </H1>
           </Header>
           <Content>
@@ -65,17 +42,6 @@ export default class Resume extends Component {
           <Header>
             <H1>
               <ScrollText title="Volunteer" />
-              {/* <Word>
-                <Letter>V</Letter>
-                <Letter>o</Letter>
-                <Letter>l</Letter>
-                <Letter>u</Letter>
-                <Letter>n</Letter>
-                <Letter>t</Letter>
-                <Letter>e</Letter>
-                <Letter>e</Letter>
-                <Letter>r</Letter>
-              </Word> */}
             </H1>
           </Header>
           <Content>
@@ -87,7 +53,6 @@ export default class Resume extends Component {
               <br />
             </P>
           </Content>
-          {/* <CurveText text="Education" position="M 80 80 Q 440 480 200 250" /> */}
         </ResumeContainer>
       </Section>
     );
@@ -114,8 +79,6 @@ const Word = styled.span`
   overflow: hidden;
   vertical-align: top;
 `;
-
-const NewWord = styled.div``;
 
 const Letter = styled.span`
   font-size: 5rem;
@@ -184,31 +147,31 @@ const ResumeTitle = styled(Word)(props => ({
   width: "100%"
 }));
 
-const ResumeTitle2 = styled(NewWord)(props => ({
-  visibility: props.percent > 0.2 ? "visible" : "hidden",
-  position: "relative",
-  transform:
-    props.percent > 0.05
-      ? "matrix(1.00,0.00,0.00,1.00,0,0)"
-      : "matrix(1.00,0.00,0.00,1.00, -125, 25)",
-  webkitTransition: "opacity 1s ease",
-  transition:
-    "opacity 1s ease , transform 0.5s ease-in-out 0s, color 0.25s ease-in-out",
-  width: "200px",
-  height: "75px"
-}));
+// const ResumeTitle2 = styled(NewWord)(props => ({
+//   visibility: props.percent > 0.2 ? "visible" : "hidden",
+//   position: "relative",
+//   transform:
+//     props.percent > 0.05
+//       ? "matrix(1.00,0.00,0.00,1.00,0,0)"
+//       : "matrix(1.00,0.00,0.00,1.00, -125, 25)",
+//   webkitTransition: "opacity 1s ease",
+//   transition:
+//     "opacity 1s ease , transform 0.5s ease-in-out 0s, color 0.25s ease-in-out",
+//   width: "200px",
+//   height: "75px"
+// }));
 
-const ResumeTitle3 = styled(NewWord)(props => ({
-  // visibility: props.percent > 0.2 ? "visible" : "hidden",
-  position: "relative"
-  // transform:
-  //   props.percent > 0.05
-  //     ? "matrix(1.00,0.00,0.00,1.00,0,0)"
-  //     : "matrix(1.00,0.00,0.00,1.00, -125, 25)",
-  // webkitTransition: "opacity 1s ease",
-  // transition:
-  //   "opacity 1s ease , transform 0.5s ease-in-out 0s, color 0.25s ease-in-out"
-}));
+// const ResumeTitle3 = styled(NewWord)(props => ({
+//   // visibility: props.percent > 0.2 ? "visible" : "hidden",
+//   position: "relative"
+//   // transform:
+//   //   props.percent > 0.05
+//   //     ? "matrix(1.00,0.00,0.00,1.00,0,0)"
+//   //     : "matrix(1.00,0.00,0.00,1.00, -125, 25)",
+//   // webkitTransition: "opacity 1s ease",
+//   // transition:
+//   //   "opacity 1s ease , transform 0.5s ease-in-out 0s, color 0.25s ease-in-out"
+// }));
 
 const SpanLetter = props => {
   const { word } = props;
@@ -218,22 +181,22 @@ const SpanLetter = props => {
   });
 };
 
-const SpanLetter3 = props => {
-  const { word } = props;
+// const SpanLetter3 = props => {
+//   const { word } = props;
 
-  return word.split("").map((letter, index) => {
-    return (
-      <Animate
-        key={letter}
-        sequenceIndex={index}
-        end={{ opacity: 1, transform: "translateX(10px)" }}
-        start={{ opacity: 0, transform: "translateX(0)" }}
-      >
-        <Letter key={index}>{letter}</Letter>
-      </Animate>
-    );
-  });
-};
+//   return word.split("").map((letter, index) => {
+//     return (
+//       <Animate
+//         key={letter}
+//         sequenceIndex={index}
+//         end={{ opacity: 1, transform: "translateX(10px)" }}
+//         start={{ opacity: 0, transform: "translateX(0)" }}
+//       >
+//         <Letter key={index}>{letter}</Letter>
+//       </Animate>
+//     );
+//   });
+// };
 
 const SpanLetter2 = props => {
   const { word } = props;
@@ -260,27 +223,27 @@ const SpanLetter2 = props => {
 //   .staggerTo(letters, 0.3, {scale:0.8}, 0.3, 0.7);
 // };
 
-const ScrollText3 = props => (
-  <ScrollPercentage>
-    {({ percentage, ref, entry }) => (
-      <ResumeTitle3 ref={ref} percent={percentage.toPrecision(2)}>
-        <AnimateGroup play={percentage.toPrecision(2) > 0.1 ? "true" : "false"}>
-          <SpanLetter3 word={props.title} />
-        </AnimateGroup>
-      </ResumeTitle3>
-    )}
-  </ScrollPercentage>
-);
+// const ScrollText3 = props => (
+//   <ScrollPercentage>
+//     {({ percentage, ref, entry }) => (
+//       <ResumeTitle3 ref={ref} percent={percentage.toPrecision(2)}>
+//         <AnimateGroup play={percentage.toPrecision(2) > 0.1 ? "true" : "false"}>
+//           <SpanLetter3 word={props.title} />
+//         </AnimateGroup>
+//       </ResumeTitle3>
+//     )}
+//   </ScrollPercentage>
+// );
 
-const ScrollText2 = props => (
-  <ScrollPercentage>
-    {({ percentage, ref, entry }) => (
-      <ResumeTitle2 ref={ref} percent={percentage.toPrecision(2)}>
-        <SpanLetter2 word={props.title} />
-      </ResumeTitle2>
-    )}
-  </ScrollPercentage>
-);
+// const ScrollText2 = props => (
+//   <ScrollPercentage>
+//     {({ percentage, ref, entry }) => (
+//       <ResumeTitle2 ref={ref} percent={percentage.toPrecision(2)}>
+//         <SpanLetter2 word={props.title} />
+//       </ResumeTitle2>
+//     )}
+//   </ScrollPercentage>
+// );
 
 const ScrollText = props => (
   <ScrollPercentage>
