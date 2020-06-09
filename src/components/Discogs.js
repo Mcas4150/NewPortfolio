664f258ab9d4cf24a42a4107566a328c7af441b4
Mcas4150 <mikecassidy0000@gmail.com>
2020-06-08 22:42:53 -0400

    working discogs

--- src/components/Discogs.js
+++ src/components/Discogs.js
@@ -1,25 +1,47 @@
 import React, { Component } from "react";
+import YouTube from "react-youtube";
 
 export default class Discogs extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      Release: ["empty"],
+      Artists: ["empty"],
+      Styles: [],
+      Genres: [],
+      Videos: [],
     };
   }
 
   componentDidMount() {
+    this.loadRelease();
+  }
+
+  loadRelease() {
     var Discogs = require("disconnect").Client;
     var db = new Discogs().database();
-    db.getRelease(176126, function (err, data) {
+    db.getRelease(176126, (err, data) => {
       console.log(data);
-      // this.setState({
-      //   Release: data,
-      // });
+      this.setState({
+        Artists: [data.artists],
+        Styles: [data.styles],
+        Genres: [data.genres],
+        Videos: [data.videos[0].uri.slice(32)],
+      });
     });
   }
 
   render() {
-    return <div>{this.state.Release}</div>;
+    const { Artists, Styles, Genres, Videos } = this.state;
+    return (
+      <div>
+        {Styles}
+        <div>
+          <YouTube videoId={Videos}></YouTube>
+        </div>
+
+        <div></div>
+        <div></div>
+      </div>
+    );
   }
 }
