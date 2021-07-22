13982fd02dc33eabd01f648c00697db117795be3
Mcas4150 <mikecassidy0000@gmail.com>
2021-07-22 19:01:43 -0400

    langton

--- src/components/animations/Processing.js
+++ src/components/animations/Processing.js
@@ -1,24 +1,96 @@
 import React from "react";
 import Sketch from "react-p5";
 
-let x = 50;
-let y = 50;
+let grid;
+let x;
+let y;
+let dir;
+
+let ANTUP = 0;
+let ANTRIGHT = 1;
+let ANTDOWN = 2;
+let ANTLEFT = 3;
+let height = 400;
+let width = 400;
 
 const Processing = (props) => {
   const setup = (p5, canvasParentRef) => {
     // use parent to render the canvas in this ref
     // (without that p5 will render the canvas outside of your component)
-    p5.createCanvas(500, 500).parent(canvasParentRef);
+    p5.createCanvas(400, 400).parent(canvasParentRef);
+    grid = make2DArray(width, height);
+    x = width / 2;
+    y = height / 2;
+    dir = ANTUP;
   };
 
   const draw = (p5) => {
-    p5.background(0);
-    p5.ellipse(x, y, 70, 70);
-    // NOTE: Do not use setState in the draw function or in functions that are executed
-    // in the draw function...
-    // please use normal variables or class properties for these purposes
-    x++;
+    p5.strokeWeight(1);
+    for (let n = 0; n < 100; n++) {
+      let state = grid[x][y];
+      if (state === 0) {
+        turnRight();
+        grid[x][y] = 1;
+      } else if (state === 1) {
+        turnLeft();
+        grid[x][y] = 0;
+      }
+
+      p5.stroke(p5.color(255));
+      if (grid[x][y] === 1) {
+        p5.stroke(p5.color(0));
+      }
+      p5.point(x, y);
+      moveForward();
+    }
   };
+  function turnRight() {
+    dir++;
+    if (dir > ANTLEFT) {
+      dir = ANTUP;
+    }
+  }
+
+  function turnLeft() {
+    dir--;
+    if (dir < ANTUP) {
+      dir = ANTLEFT;
+    }
+  }
+
+  function moveForward() {
+    if (dir === ANTUP) {
+      y--;
+    } else if (dir === ANTRIGHT) {
+      x++;
+    } else if (dir === ANTDOWN) {
+      y++;
+    } else if (dir === ANTLEFT) {
+      x--;
+    }
+
+    if (x > width - 1) {
+      x = 0;
+    } else if (x < 0) {
+      x = width - 1;
+    }
+    if (y > height - 1) {
+      y = 0;
+    } else if (y < 0) {
+      y = height - 1;
+    }
+  }
+
+  function make2DArray(cols, rows) {
+    let arr = new Array(cols);
+    for (let i = 0; i < arr.length; i++) {
+      arr[i] = new Array(rows);
+      for (let j = 0; j < arr[i].length; j++) {
+        arr[i][j] = 0;
+      }
+    }
+    return arr;
+  }
 
   return <Sketch setup={setup} draw={draw} />;
 };
