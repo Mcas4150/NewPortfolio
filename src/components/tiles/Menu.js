783dd067376e7a6ddaf53b659e36e61112f69e06
Mcas4150 <mikecassidy0000@gmail.com>
2021-07-23 20:18:44 -0400

    menu styling

--- src/components/tiles/Menu.js
+++ src/components/tiles/Menu.js
@@ -7,31 +7,54 @@ function Menu(props) {
   const context = useContext(AppContext);
 
   return (
-    <MenuContainer
-      style={
-        context.dimensions.width <= 800 || context.dimensions.height <= 600
-          ? MenuStyleMin
-          : MenuStyleMax
-      }
-    >
-      <MenuLink to={{ pathname: `/about` }} onClick={context.toggleWindow}>
-        About
-      </MenuLink>
+    <MenuContainer>
+      <MenuCategory>
+        <CategoryLink>Projects</CategoryLink>
+        <LinkContainer>
+          <MenuLink to={{ pathname: `/projects/ocean-memory` }}>
+            Ocean Memory
+          </MenuLink>
+          <MenuLink to={{ pathname: `/projects/instruments` }}>
+            Instrument Building
+          </MenuLink>
+          <MenuLink to={{ pathname: `/projects/software` }}>Software</MenuLink>
+        </LinkContainer>
+      </MenuCategory>
+      <MenuCategory>
+        <CategoryLink>Works</CategoryLink>
+        <LinkContainer>
+          <MenuLink to={{ pathname: `/works/electroacoustics` }}>
+            Electroacoustics
+          </MenuLink>
+          <MenuLink to={{ pathname: `/works/animation` }}>Animation</MenuLink>
+          <MenuLink to={{ pathname: `/works/sound-design` }}>
+            Sound Design
+          </MenuLink>
+        </LinkContainer>
+      </MenuCategory>
 
-      <MenuLink to={{ pathname: `/projects` }} onClick={context.toggleWindow}>
-        Projects
-      </MenuLink>
-
-      <MenuLink to={{ pathname: `/works` }}>
-        Works
-      </MenuLink>
+      <CategoryLink to={{ pathname: `/about` }}>About</CategoryLink>
     </MenuContainer>
   );
 }
+const MenuCategory = styled.div``;
+
+const CategoryLink = styled(Link)`
+  text-decoration: none;
+  font-family: Verdana;
+  font-weight: bold;
+  color: black;
+`;
+
+const LinkContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 const MenuLink = styled(Link)`
   text-decoration: none;
   font-family: Verdana;
+  color: black;
   // font-size: 1rem;
   // font-size: 25px;
   // color:  ;
@@ -40,15 +63,7 @@ const MenuLink = styled(Link)`
 const MenuContainer = styled.div`
   margin: 25px;
   display: flex;
+  flex-direction: column;
 `;
-
-const MenuStyleMax = {
-  flexDirection: "column",
-};
-
-const MenuStyleMin = {
-  flexDirection: "row",
-  justifyContent: "space-between",
-};
 
 export default Menu;
