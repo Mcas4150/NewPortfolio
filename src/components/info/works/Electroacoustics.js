dab1b1b08cb6c93c56feeb74b17898f09c3648f1
Mcas4150 <mikecassidy0000@gmail.com>
2021-07-26 17:43:39 -0400

    soundcloud

--- src/components/info/works/Electroacoustics.js
+++ src/components/info/works/Electroacoustics.js
@@ -1,11 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Electroacoustics extends Component {
   render() {
     return (
       <div>
-        https://soundcloud.com/charmingultra
+        <iframe
+          width="100%"
+          height="166"
+          scrolling="no"
+          frameborder="no"
+          allow="autoplay"
+          src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/887079559&color=%23cfccc6&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
+        ></iframe>
+        <iframe
+          width="100%"
+          height="166"
+          scrolling="no"
+          frameborder="no"
+          allow="autoplay"
+          src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1039615969&color=%2397a3b1&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
+        ></iframe>
       </div>
-    )
+    );
   }
 }
